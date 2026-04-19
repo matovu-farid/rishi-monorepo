d04b553b92dca440973f44f58cba99bf323ba76d
matovu-farid <matovu90@gmail.com>
2026-04-19 11:45:17 +0300

    feat: add PageCurlOverlay component and pagecurl barrel exports

---
+++ apps/main/src/components/pagecurl/index.ts
@@ -0,0 +1,5 @@
+// apps/main/src/components/pagecurl/index.ts
+
+export { PageCurlOverlay } from "./PageCurlOverlay";
+export { usePageCurl } from "./usePageCurl";
+export type { CurlDirection } from "./drawPageCurl";
